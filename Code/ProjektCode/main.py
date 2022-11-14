cae0b135405d5fa38ec777c6250eea00273a915a
JupiterXVI <gehrmannjakob@gmail.com>
2022-11-14 08:46:07 +0100

    allow debugger to start from mein.py

--- Code/ProjektCode/main.py
+++ Code/ProjektCode/main.py
@@ -11,10 +11,11 @@ def test_from_import_core_files():
 def test_from_import_adapter():
     get_connection()
 
-if __name__ == "__main__":
-    print("started main file...")
-    #test_from_import_core_files()
-    #test_from_import_adapter()
-    menu = Menu(600, 600)
-    library = GameLibrary(menu)
-    library.open_main_menu()
+
+
+#test_from_import_core_files()
+#test_from_import_adapter()
+print("started main file...")
+menu = Menu(600, 600)
+library = GameLibrary(menu)
+library.open_main_menu()
