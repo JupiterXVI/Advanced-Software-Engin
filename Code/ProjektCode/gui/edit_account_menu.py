8ee6bf3cdc8efc85b9f781272248f1fecf4e7159
JupiterXVI <gehrmannjakob@gmail.com>
2023-01-09 23:23:35 +0100

    test is editable

--- Code/ProjektCode/gui/edit_account_menu.py
+++ Code/ProjektCode/gui/edit_account_menu.py
@@ -2,6 +2,8 @@ from os import path as os_path
 from sys import path as sys_path
 sys_path.append(os_path.join(sys_path[0], '..'))
 
+import re
+
 from adapter import Menu
 from adapter import AllowToBuldMenu
 from adapter import Timeable
@@ -15,16 +17,30 @@ class EditAccountMenu(Menu):   # mit vererbung könnten die __init__, open- clos
         self.menu_interactables = "list of interactables"
 
         #problems:
-        # live ändern der rectangel texte
-        # text eingbe mit events
         # einholen der werte von Account
         # speichern der Werte
 
     def get_account_value():
         pass
 
-    def enter_text(self):
-        pass
+    def enter_text(self, text_field):
+        self.timer.blocking_wait_milliseconds(800)
+        still_tiyping = True
+        while still_tiyping:
+            action = self.gui.check_events(self.menu_interactables)
+            self.gui.set_element_styles()
+            self.gui.update_window()
+            self.timer.allow_passes_per_second(90)
+            if action == "quit":               # kommt man von diesem if wald weg?
+                still_tiyping = False
+            if re.search("^.$", action):
+                text_field["text"]["content"] = text_field["text"]["content"]+action
+                continue
+            if action == "backspace":
+                text_field["text"]["content"] = text_field["text"]["content"][:-1]
+                continue
+            if action != "no action":
+                still_tiyping = False
 
 
     def open_menu(self):
@@ -44,15 +60,15 @@ class EditAccountMenu(Menu):   # mit vererbung könnten die __init__, open- clos
                 if action == "quit":               # kommt man von diesem if wald weg?
                     choose_menu_active = False
                 if action == "input_username":
-                    self.enter_text()
+                    self.enter_text(EditAccount.input_username)
                 if action == "input_password":
-                    self.enter_text()
+                    self.enter_text(EditAccount.input_password)
                 if action == "input_password_repeat":
-                    self.enter_text()
+                    self.enter_text(EditAccount.input_password_repeat)
                 if action == "input_age":
-                    self.enter_text()
+                    self.enter_text(EditAccount.input_age)
                 if action == "input_admin":
-                    self.enter_text()
+                    self.enter_text(EditAccount.input_admin)
                 if action == "save_button":
                     print("save")
                     choose_menu_active = False
